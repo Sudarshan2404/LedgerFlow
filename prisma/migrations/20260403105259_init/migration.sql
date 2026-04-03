dc33bad0cc644e5474cf3f93ed5e5e3f8f5231d7
Sudarshan Kulkarni <Sudarshan.kul2404@gmail.com>
2026-04-03 17:04:15 +0530

    Initialized prisma

---
+++ prisma/migrations/20260403105259_init/migration.sql
@@ -0,0 +1,24 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "role" TEXT NOT NULL,
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- CreateTable
+CREATE TABLE "Records" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "amount" REAL NOT NULL,
+    "type" TEXT NOT NULL,
+    "category" TEXT NOT NULL,
+    "date" DATETIME NOT NULL,
+    "notes" TEXT,
+    "userId" TEXT NOT NULL,
+    CONSTRAINT "Records_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
