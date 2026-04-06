431f0753173cf6a29fb521b901e82aedb321809c
Sudarshan Kulkarni <Sudarshan.kul2404@gmail.com>
2026-04-06 13:54:09 +0530

    created more controllers to get records

--- src/controllers/crudControllers.ts
+++ src/controllers/crudControllers.ts
@@ -12,6 +12,87 @@ declare global {
   }
 }
 
+export const getAllRecords = async (req: Request, res: Response) => {
+  try {
+    const userId = req.userId;
+
+    if (!userId) {
+      return res.status(401).json({
+        success: false,
+        message: "Unauthorized",
+      });
+    }
+
+    const records = await prisma.records.findMany({
+      where: {
+        userId,
+      },
+      orderBy: {
+        date: "desc",
+      },
+    });
+
+    return res.status(200).json({
+      success: true,
+      count: records.length,
+      data: records,
+    });
+  } catch (error) {
+    console.error("Error fetching records:", error);
+
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
+
+export const getRecordbyId = async (req: Request, res: Response) => {
+  try {
+    const userId = req.userId;
+    const { id } = req.params;
+
+    if (!userId) {
+      return res.status(401).json({
+        success: false,
+        message: "Unauthorized",
+      });
+    }
+
+    if (!id) {
+      return res.status(400).json({
+        success: false,
+        message: "Please provide record id",
+      });
+    }
+
+    const record = await prisma.records.findFirst({
+      where: {
+        id: id as string,
+      },
+    });
+
+    if (!record) {
+      return res.status(404).json({
+        success: false,
+        message: "Record not found",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      data: record,
+    });
+  } catch (error) {
+    console.error("Error fetching records:", error);
+
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
+
 export const addRecord = async (req: Request, res: Response) => {
   try {
     const { amount, type, category, notes } = req.body;
@@ -62,39 +143,90 @@ export const updateRecord = async (req: Request, res: Response) => {
     const date = new Date();
     const userId = req.userId;
 
+    if (!recordId) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Please enter the record id" });
+    }
+
     if (!userId) {
       return res.status(401).json({
         success: false,
         message: "Unauthorized",
       });
     }
+    const existing = await prisma.records.findFirst({
+      where: { id: recordId },
+    });
+
+    if (existing?.userId != userId) {
+      return res.status(400).json({
+        success: false,
+        message:
+          "This record cannot be updated as it is not associated with you",
+      });
+    }
 
     const record = await prisma.records.update({
-      where: { id: recordId, userId },
+      where: { id: recordId },
       data: {
         amount,
         type,
         category,
         notes,
         date,
+        user: {
+          connect: { id: userId },
+        },
       },
     });
 
-    if (record.userId != userId) {
-      return res
-        .status(400)
-        .json({
-          success: false,
-          message:
-            "This record cannot be updated as it is not associated with you",
-        });
-    }
     return res.status(200).json({
       success: true,
       message: "Record updated successfully",
     });
   } catch (error) {
     console.error("An error occurred while updating the record", error);
+    res.status(500).json({ success: false, message: "Internal server Error" });
+  }
+};
+
+export const deleteRecord = async (req: Request, res: Response) => {
+  try {
+    const userId = req.userId;
+    const { recordId } = req.body;
+    if (!userId) {
+      return res.status(401).json({
+        success: false,
+        message: "Unauthorized",
+      });
+    }
+    const existing = await prisma.records.findFirst({
+      where: { id: recordId },
+    });
+
+    if (!existing) {
+      res.status(404).json({ success: false, message: "recoed not found" });
+    }
+
+    if (existing?.userId != userId) {
+      return res.status(400).json({
+        success: false,
+        message:
+          "This record cannot be deleted as it is not associated with you",
+      });
+    }
+
+    await prisma.records.delete({
+      where: { id: recordId },
+    });
+
+    return res.status(200).json({
+      success: true,
+      message: "Record deleted successfully",
+    });
+  } catch (error) {
+    console.error("An error occurred while deleting the record", error);
     res.status(500).json({ success: false, message: "Internal server Error" });
   }
 };
--- src/routes/firecords.ts
+++ src/routes/firecords.ts
@@ -1,11 +1,21 @@
 import { Router } from "express";
 import { login, register } from "../controllers/authControllers.js";
 import { AdminRole } from "../middleware/RoleMiddleware.js";
-import { addRecord } from "../controllers/crudControllers.js";
+import {
+  addRecord,
+  deleteRecord,
+  getAllRecords,
+  getRecordbyId,
+  updateRecord,
+} from "../controllers/crudControllers.js";
 import { Authmiddleware } from "../middleware/authmiddleware.js";
 
 const router = Router();
 
+router.get("/records", Authmiddleware, getAllRecords);
+router.get("/getrecord/:id", Authmiddleware, getRecordbyId);
 router.post("/addrecords", Authmiddleware, AdminRole, addRecord);
+router.patch("/updaterecord", Authmiddleware, AdminRole, updateRecord);
+router.delete("/deleterecord", Authmiddleware, AdminRole, deleteRecord);
 
 export default router;
